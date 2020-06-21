8bf66a66ef2a476d73d54b3e6097615c14ed3ccf
Gideon Okuro <gideonollonde@gmail.com>
2020-06-21 17:46:11 +0300

    add a serach form component and create a form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileService } from './profile.service';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
